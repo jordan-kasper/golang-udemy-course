cd9ff9c97d261333162923ce69d0e96ee0ef4454
Jordan Kasper <jordan.kasper@gmail.com>
2025-04-21 09:53:32 -0700

    first exercise of course

---
+++ section-003/exercise-1.go
@@ -0,0 +1,17 @@
+/* 
+Exercise 1
+- Print out a some text with emojis
+- Print out a raw string literal
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, 👋🌍 ")
+	fmt.Println(`This is a raw string literal
+					with multiple
+					lines of text.
+	`)
+}
